bf580eb7d8935f2818e2a416a2ccc56a957b3d5a
maulanaaris <maulanaaris221@gmail.com>
2018-05-12 17:42:12 +0700

    add image listview

--- androidstudio-project/Ambulance/app/src/main/java/com/example/dell/ambulance/ambulance/AmbulanceFragment.java
+++ androidstudio-project/Ambulance/app/src/main/java/com/example/dell/ambulance/ambulance/AmbulanceFragment.java
@@ -2,15 +2,18 @@ package com.example.dell.ambulance.ambulance;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.dell.ambulance.R;
+import com.example.dell.ambulance.information.DetailInfoActivity;
 
 import java.util.ArrayList;
 
@@ -30,14 +33,28 @@ public class AmbulanceFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         ArrayList<AmbulanceList> Ambulance = new ArrayList<>();
-        for(int i = 0;i<10 ;i++)
-        {
-            Ambulance.add(new AmbulanceList("Nama Ambulance"+(i+1),"Rp 1000","Alamat"+(i+1),R.drawable.ic_launcher_background));
-        }
+        Ambulance.add(new AmbulanceList("Nama Ambulance","Rp 1000","Alamat",R.drawable.nama1));
+        Ambulance.add(new AmbulanceList("Nama Ambulance","Rp 2000","Alamat",R.drawable.nama2));
+        Ambulance.add(new AmbulanceList("Nama Ambulance","Rp 3000","Alamat",R.drawable.nama3));
+        Ambulance.add(new AmbulanceList("Nama Ambulance","Rp 4000","Alamat",R.drawable.nama4));
+        Ambulance.add(new AmbulanceList("Nama Ambulance","Rp 5000","Alamat",R.drawable.nama2));
+        Ambulance.add(new AmbulanceList("Nama Ambulance","Rp 6000","Alamat",R.drawable.nama6));
+        Ambulance.add(new AmbulanceList("Nama Ambulance","Rp 7000","Alamat",R.drawable.nama7));
+        Ambulance.add(new AmbulanceList("Nama Ambulance","Rp 8000","Alamat",R.drawable.nama8));
+        Ambulance.add(new AmbulanceList("Nama Ambulance","Rp 9000","Alamat",R.drawable.nama2));
+        Ambulance.add(new AmbulanceList("Nama Ambulance","Rp 4000","Alamat",R.drawable.nama10));
+
         AmbulanceAdapter ambulanceAdapter = new AmbulanceAdapter(getContext(),Ambulance);
 
         ListView listView =(ListView) view.findViewById(R.id.listAmbulance);
         listView.setAdapter(ambulanceAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(getContext(), DetailAmbulanceActivity.class));
+            }
+        });
+
     }
 
     @Override
--- androidstudio-project/Ambulance/app/src/main/java/com/example/dell/ambulance/information/InfoFragment.java
+++ androidstudio-project/Ambulance/app/src/main/java/com/example/dell/ambulance/information/InfoFragment.java
@@ -2,12 +2,14 @@ package com.example.dell.ambulance.information;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import com.example.dell.ambulance.R;
@@ -31,14 +33,26 @@ public class InfoFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         ArrayList<InfoList> Info = new ArrayList<>();
-        for(int i = 0;i<10 ;i++)
-        {
-            Info.add(new InfoList("Kejadian"+(i+1),"Lokasi",(i+1)+"-"+(i+1)+"-2018",R.drawable.ic_launcher_background));
-        }
+        Info.add(new InfoList("Kejadian 1","Lokasi","1-1-2018",R.drawable.kecelakaan1));
+        Info.add(new InfoList("Kejadian 2","Lokasi","2-2-2018",R.drawable.kecelakaan2));
+        Info.add(new InfoList("Kejadian 3","Lokasi","3-3-2018",R.drawable.kecelakaan3));
+        Info.add(new InfoList("Kejadian 4","Lokasi","4-4-2018",R.drawable.kecelakaan4));
+        Info.add(new InfoList("Kejadian 5","Lokasi","5-5-2018",R.drawable.kecelakaan6));
+        Info.add(new InfoList("Kejadian 6","Lokasi","7-7-2018",R.drawable.kecelakaan7));
+        Info.add(new InfoList("Kejadian 7","Lokasi","1-1-2018",R.drawable.kecelakaan8));
+        Info.add(new InfoList("Kejadian 8","Lokasi","6-6-2018",R.drawable.kecelakaan3));
+        Info.add(new InfoList("Kejadian 9","Lokasi","4-4-2018",R.drawable.kecelakaan6));
+
         InfoAdapter infoAdapter = new InfoAdapter(getContext(),Info);
 
         ListView listView =(ListView) view.findViewById(R.id.listInfo);
         listView.setAdapter(infoAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(getContext(), DetailInfoActivity.class));
+            }
+        });
     }
 
     @Override
